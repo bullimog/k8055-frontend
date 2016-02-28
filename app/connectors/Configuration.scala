bf5b6412a24c50893fd97b1332baa66283304e46
bullimog <gmb@bullimore.com>
2016-02-28 17:19:41 +0000

    changed big inc and dec for % refactor

--- app/connectors/Configuration.scala
+++ app/connectors/Configuration.scala
@@ -8,9 +8,9 @@ trait Configuration {
   val k8055DeviceStateDelta = Play.current.configuration.getString("k8055.deviceStateDelta").fold("badDeviceConfig") (filename => filename)
 
   val increment = Play.current.configuration.getInt("increment.single").fold(1) (increment => increment)
-  val big_increment = Play.current.configuration.getInt("increment.big").fold(10) (increment => increment)
+  val big_increment = Play.current.configuration.getInt("increment.big").fold(25) (increment => increment)
   val decrement = Play.current.configuration.getInt("decrement.single").fold(-1) (decrement => decrement)
-  val big_decrement = Play.current.configuration.getInt("decrement.big").fold(-10) (decrement => decrement)
+  val big_decrement = Play.current.configuration.getInt("decrement.big").fold(-25) (decrement => decrement)
 
   val up = "up"
   val doubleUp = "dup"
